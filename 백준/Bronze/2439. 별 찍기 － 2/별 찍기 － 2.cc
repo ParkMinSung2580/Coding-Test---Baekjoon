5e3b3852be04b3c0488b488d905084a515f5de89
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2025-04-10 17:28:24 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int num;
+    cin >> num;
+
+    for (int i = 1; i <= num; i++) {
+        // 공백 출력
+        for (int j = 1; j <= num - i; j++) {
+            cout << ' ';
+        }
+        // 별 출력
+        for (int k = 1; k <= i; k++) {
+            cout << '*';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
