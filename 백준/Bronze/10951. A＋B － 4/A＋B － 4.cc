de02341e024d369fcb41a22d6cedbf4d7c1fc18e
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2026-02-19 11:38:54 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int A, B;
+
+	while (true)
+	{
+		cin >> A >> B;
+		if (cin.eof() == true)
+			break;
+
+		cout << A + B << "\n";
+	}
+}
