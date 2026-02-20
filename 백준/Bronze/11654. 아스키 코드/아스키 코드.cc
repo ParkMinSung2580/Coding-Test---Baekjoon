e06053e9b1a4358545a23e8d5d524bedceea27fc
ParkMinSung2580 <114042812+ParkMinSung2580@users.noreply.github.com>
2026-02-20 16:44:43 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    char c;
+    cin >> c;
+
+    cout << (int)c << "\n";
+    //cout << static_cast<int>(c) << "\n";
+}
